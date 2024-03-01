3dc16bb0b27403c2d14c2c947f496cff9ad7a062
myusername <jaatsaab735@gmail.com>
2024-03-01 21:27:27 +0530

    rider

--- server/server.js
+++ server/server.js
@@ -9,11 +9,26 @@ const adminRoute=require("./router/admin-router");
 const connectDB=require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 const corsOptions={
-    // origin:"http://localhost:5173",
-    origin:"http://ridescom.com",
+    origin:(origin,callback)=>{
+        const allowedOrigins=[
+            "http://localhost:5173",
+            "http://localhost:4173",
+            "https://ridescom.com",
+            "https://www.ridescom.com",
+            "https://api.ridescom.com",
+            "https://api.www.ridescom.com",
+        ];
+        const isAllowed=allowedOrigins.includes(origin);
+        callback(null,isAllowed ? origin: false);
+
+    },
     methods:"GET, POST, PUT,DELETE,PATCH,HEAD",
     credentials:true,
-}
+};
+
+
+    // origin:"http://localhost:5173",
+    // // origin:"http://ridescom.com",
 
 app.use(cors(corsOptions));
 app.use(express.json());
